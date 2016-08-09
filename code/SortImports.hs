b9118ca87591856ae9cb74ec20302123a800149b
Andrzej Rybczak <electricityispower@gmail.com>
2016-08-09 16:47:18 +0200

    SortImport: use proper parser

--- code/SortImports.hs
+++ code/SortImports.hs
@@ -1,20 +1,21 @@
-{-# LANGUAGE BangPatterns, OverloadedStrings #-}
+{-# LANGUAGE BangPatterns, LambdaCase, OverloadedStrings, RecordWildCards #-}
 {-# OPTIONS_GHC -Wall #-}
 module Main (main) where
 
 import Control.Applicative
-import Control.Arrow
 import Control.Exception
 import Control.Monad
 import Data.Char
-import Data.Function
+import Data.Functor
 import Data.List
+import Data.Monoid
 import Data.Set (Set)
 import Data.Text (Text)
 import System.Directory
 import System.Environment
 import System.Exit
 import System.IO (stderr, hPutStrLn)
+import qualified Data.Attoparsec.Text as P
 import qualified Data.Set as S
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
@@ -27,6 +28,100 @@ infixr 3 <&&>
 (<||>) = liftA2 (||)
 infixr 2 <||>
 
+parenthesize :: [T.Text] -> T.Text
+parenthesize ss = "(" <> T.intercalate ", " ss <> ")"
+
+----------------------------------------
+
+data Symbols
+  = NoSymbols
+  | Explicit ![T.Text]
+  | Hiding ![T.Text]
+  deriving (Eq, Ord, Show)
+
+parseSymbols :: P.Parser Symbols
+parseSymbols = P.choice [
+    Hiding   <$> (P.string "hiding" *> P.skipSpace *> parseSymbolList)
+  , Explicit <$> parseSymbolList
+  , return NoSymbols
+  ]
+  where
+    parseSymbolList = do
+      _ <- P.char '('
+      P.skipSpace
+      symbols <- (`P.sepBy` P.char ',') $ do
+        P.skipSpace
+        symbol <- P.peekChar' >>= \case
+          '(' -> do
+            _ <- P.char '('
+            op <- P.takeWhile1 (/= ')')
+            _ <- P.char ')'
+            return $ "(" <> op <> ")"
+          _ -> P.takeWhile1 ((not . isSpace) <&&> (/= '(') <&&> (/= ')') <&&> (/= ','))
+        P.skipSpace
+        ctors <- P.option "" (parenthesize <$> parseSymbolList)
+        P.skipSpace
+        return $ symbol <> ctors
+      P.skipSpace
+      _ <- P.char ')'
+      P.skipSpace
+      return $ sort symbols
+
+data Import = Import {
+    imQualified      :: !Bool
+  , imModule         :: !T.Text
+  , imCaselessModule :: !T.Text
+  , imAlias          :: !(Maybe T.Text)
+  , imSymbols        :: !Symbols
+  } deriving (Eq, Show)
+
+compareImport :: Import -> Import -> Ordering
+compareImport a b = mconcat [
+    imQualified a      `compare` imQualified b
+  , imCaselessModule a `compare` imCaselessModule b
+  , imAlias a          `compare` imAlias b
+  , imSymbols a        `compare` imSymbols b
+  ]
+
+parseImport :: P.Parser Import
+parseImport = do
+  _ <- P.string "import"
+  P.skipSpace
+  is_qualified <- P.option False (P.string "qualified" *> P.skipSpace $> True)
+  module_ <- P.takeWhile1 $ (not . isSpace) <&&> (/= '(')
+  P.skipSpace
+  alias <- P.option Nothing (Just <$> parseAlias)
+  P.skipSpace
+  symbols <- parseSymbols
+  return Import {
+      imQualified      = is_qualified
+    , imModule         = module_
+    , imCaselessModule = T.toCaseFold module_
+    , imAlias          = alias
+    , imSymbols        = symbols
+    }
+  where
+    parseAlias :: P.Parser T.Text
+    parseAlias = do
+      _ <- P.string "as"
+      P.skipSpace
+      alias <- P.takeWhile1 $ (not . isSpace) <&&> (/= '(')
+      P.skipSpace
+      return alias
+
+showImport :: Import -> T.Text
+showImport Import{..} = T.concat [
+    "import"
+  , if imQualified then " qualified" else ""
+  , " "
+  , imModule
+  , maybe "" (" as " <>) imAlias
+  , case imSymbols of
+      NoSymbols   -> ""
+      Explicit ss -> " " <> parenthesize ss
+      Hiding ss   -> " hiding " <> parenthesize ss
+  ]
+
 ----------------------------------------
 
 -- | Transform the haskell source file by lexicographically sorting
@@ -36,42 +131,28 @@ convert :: Set Text -> Text -> Text
 convert modules source = T.unlines . concat $ [
     reverse . dropWhile T.null $ reverse header
   , [T.empty]
-  , fimports
-  , fqimports
+  , map showImport foreign_imports
   , separator_if has_foreign_imports
-  , limports
-  , lqimports
+  , map showImport local_imports
   , separator_if has_local_imports
   , rest
   ]
   where
-    (header, body) = break (is import_) . T.lines $ source
-    (import_section, rest) = break (not . (T.null <||> is import_)) body
+    (header, body) = break is_import . T.lines $ source
+    (import_section, rest) = break (not . (T.null <||> is_import)) body
 
-    (qimports, imports) = (icase_sort *** icase_sort)
-      . partition (is qualified_import_)
-      . filter (is import_)
-      $ import_section
+    imports = case P.parseOnly (many parseImport) (T.unlines import_section) of
+      Right imps -> sortBy compareImport imps
+      Left  msg  -> error msg
 
-    (limports, fimports) = partition (is_local import_) imports
-    (lqimports, fqimports) = partition (is_local qualified_import_) qimports
+    (local_imports, foreign_imports) = partition ((`S.member` modules) . imModule) imports
 
-    has_foreign_imports = not $ null fimports && null fqimports
-    has_local_imports = not $ null limports && null lqimports
+    is_import = T.isPrefixOf "import "
+
+    has_foreign_imports = not $ null foreign_imports
+    has_local_imports = not $ null local_imports
 
     separator_if p = if p then [T.empty] else []
-
-    import_ = "import "
-    qualified_import_ = "import qualified "
-    is import_type = (import_type `T.isPrefixOf`)
-
-    icase_sort = map snd
-      . sortBy (compare `on` fst)
-      . map (T.toLower &&& id . T.unwords . T.words)
-
-    is_local import_type = flip S.member modules
-      . T.takeWhile (isAlphaNum <||> (== '.'))
-      . T.drop (T.length import_type)
 
 -- | Recursively traverse the directory and pass all
 -- haskell source files into accumulating function.
